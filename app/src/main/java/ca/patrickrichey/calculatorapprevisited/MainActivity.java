70b0f2cc2933065c1cb205190ccbd9bf1b9ae5f7
bradhave94 <bradleyhaveman@yahoo.ca>
2016-03-11 11:58:26 -0500

    Worked on layout. FInished mostly. Started Code.

--- app/src/main/java/ca/patrickrichey/calculatorapprevisited/MainActivity.java
+++ app/src/main/java/ca/patrickrichey/calculatorapprevisited/MainActivity.java
@@ -8,45 +8,85 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //instantiate variables for calculations
+    float firstNumber = 0;
+    float secondNumber = 0;
+    float result = 0;
+    char operation = ' ';
+    boolean isTypingNumber = false;
+    int decimalCount = 0;
+    int operatorCount = 0;
+    private TextView resultText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        resultText = (TextView)findViewById(R.id.textView);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    //capture event when a number button is clicked
+    public void numberClicked(View view) {
+        Button number = (Button)view;
+        //while use is still entering numbers, concat to existing number
+        if (isTypingNumber) {
+            resultText.setText(resultText.getText().toString() + number.getText().toString());
+        } else {
+            resultText.setText(number.toString());
+            decimalCount = 0;
         }
+        isTypingNumber = true;
+    }
 
-        return super.onOptionsItemSelected(item);
+    //add decimal to current number
+    public void decimalClicked(View view) {
+        decimal = sender.currentTitle;
+        //if user is typing a number and hasn't yet entered a decimal point
+        if (isTypingNumber) && (decimalCount == 0) {
+            resultText.text = resultText.text + decimal;
+            decimalCount = 1; //to limit allowed decimals per number
+        }
+    }
+
+    //assign the operator on click (working on multiple operator clicks)
+    public void operationClicked(View view) {
+        if (operatorCount == 0) {
+            firstNumber = (resultText.text as NSString).floatValue;
+                    operation = sender.currentTitle;
+                    isTypingNumber = false;
+        }
+        operatorCount++;
+    }
+
+    //perform calculation
+    public void evaluate() {
+        secondNumber = (resultText.text as NSString).floatValue
+                isTypingNumber = false;
+        //perform any chosen operation
+        if (operation.equals("+")) {
+            result = firstNumber + secondNumber;
+        } elseif (operation.equals("-")){
+            result = firstNumber - secondNumber;
+        } elseif (operation.equals("x")){
+            result = firstNumber * secondNumber;
+        } elseif (operation.equals("÷")){
+            result = firstNumber / secondNumber;
+        }
+        //display the result to the resultText
+        resultText.text = "\(result)";
+        operatorCount = 0;
+    }
+
+    //clear the resultText and reset numbers
+    public void clearView() {
+        firstNumber = 0;
+        secondNumber = 0;
+        result = 0;
+        resultText.text = "\(0)";
     }
 }
